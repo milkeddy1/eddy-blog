50ce785536a6096100410056c2c99bb026fe26ad
Eddy <milkeddy1@dailyview.tw>
2023-03-18 20:04:02 +0800

    feat: layout

---
+++ src/containers/Layout/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import NavBar from './NavBar';
+
+
+export interface LayoutProps {
+    children: JSX.Element
+}
+
+export default function Layout(props: LayoutProps) {
+    return (
+        <div>
+            <NavBar />
+            <div className='bg-gray-800 '>
+                {props.children}
+            </div>
+        </div>
+    );
+}

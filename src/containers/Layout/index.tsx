17ef97a460f470c5dcaf0b69dbc8e649ed7822e4
Eddy <milkeddy1@dailyview.tw>
2023-04-04 22:31:22 +0800

    feat: page route loading handle

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -2,18 +2,29 @@ import * as React from 'react';
 import NavBar from './NavBar';
 import Footer from "./Footer"
 import { useColorMode } from '@/store';
+import clsx from 'clsx';
 export interface LayoutProps {
-  children: JSX.Element
+  children: JSX.Element,
+  pageLoading: Boolean
 }
 
 export default function Layout(props: LayoutProps) {
+  const { children, pageLoading } = props
   const { mode: colorMode }: { mode: string } = useColorMode()
+  const isDarkColor = colorMode === 'dark'
   return (
-    <div className={`min-h-[100vh] ${colorMode === 'dark' ? 'bg-gray-800' : 'bg-white'}`}>
+    <div className={clsx(
+      "min-h-[100vh]",
+      {
+        'bg-gray-800': isDarkColor,
+        'bg-white': !isDarkColor,
+        'opacity-40': pageLoading
+      }
+    )}>
       <div className='max-w-5xl mx-auto'>
         <NavBar />
         <div>
-          {props.children}
+          {children}
         </div>
         <Footer colorMode={colorMode} />
       </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from "next/router"
 import '@/styles/globals.css'
 import { Montserrat } from 'next/font/google'
 import type { AppProps } from 'next/app'
@@ -5,7 +7,24 @@ import { Layout } from '@/containers'
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout>
+
+  const router = useRouter()
+  const [pageLoading, setPageLoading] = useState(false)
+
+  useEffect(() => {
+
+    router.events.on('routeChangeStart', () => { setPageLoading(true) })
+    router.events.on('routeChangeComplete', () => { setPageLoading(false) })
+
+    return () => {
+      router.events.off('routeChangeStart', () => { setPageLoading(true) })
+      router.events.off('routeChangeComplete', () => { setPageLoading(false) })
+    }
+
+  }, [router])
+
+
+  return <Layout pageLoading={pageLoading}>
     <main className={montserrat.className}>
       <Component {...pageProps} />
     </main>

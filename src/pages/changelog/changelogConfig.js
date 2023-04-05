7bf506947debd262a48e864bd8ee2944cac1d8e7
Eddy <milkeddy1@dailyview.tw>
2023-04-05 19:25:44 +0800

    feat: change log

---
+++ src/pages/changelog/changelogConfig.js
@@ -0,0 +1,85 @@
+import arizona from "@/assets/changelogs/arizona.jpg"
+import guitar from "@/assets/changelogs/guitar.jpg"
+import graduate from "@/assets/changelogs/graduate.jpg"
+import military from "@/assets/changelogs/military.jpg"
+import project_1 from "@/assets/changelogs/project-1.PNG"
+import project_2 from "@/assets/changelogs/project-2.PNG"
+import project_3 from "@/assets/changelogs/project-3.PNG"
+
+
+const changelogConfig = [
+  {
+    date: '2013-2014年',
+    title: 'Exchange student in Arizona',
+    description: '一種全新的體驗，接觸不同的文化，給了我更不一樣的視野。',
+    img: arizona
+  },
+  {
+    date: '2016年',
+    title: '進入虎尾科技大學',
+    description: '進入大學，尚未有明確的目標以及興趣，選擇了飛機工程系已較穩定求職為主的選擇。'
+  },
+  {
+    date: '2016 - 2018',
+    title: '吉他社生活',
+    description: '很喜歡唱歌彈吉他，在大學也選擇了吉他社並積極參與表演。',
+    img: guitar
+  },
+  {
+    date: '2020/6',
+    title: '從虎尾科科技大學畢業',
+    description: '對於飛機工程領域，心裡知道不是自己喜歡的東西，僅僅是拿到文憑為目標。',
+    img: graduate
+  },
+  {
+    date: '2020/7 - 2020/11',
+    title: '當兵',
+    description: '花了不少時間去思考人生，也在這期間看了不少書，最大的收穫是交了不少好朋友。',
+    img: military
+  },
+  {
+    date: '2021/2',
+    title: '開啟前端工程師之路 !!',
+    description: '在畢業之後嘗試了不同類型的工作後，偶然接觸到這個職位，從發現到了解再到嘗試後認為這是我工作心之所向。',
+  },
+  {
+    date: '2021/4 - 2021/7',
+    title: '網頁前端工程師實戰班',
+    description: '通過線上課程自學一段時間後，決定了這條路，參加了職訓局所開的"網頁前端工程師實戰班"，對於上完課的心得是不認為有能馬上就職的程度，但是確實讓我對這個產業有初步的了解，很有助於後續我自學的方向。'
+  },
+  {
+    date: '2021/6 - 2021/7',
+    title: '網頁前端工程師實戰班 - 作品',
+    description: '在課程中學習最多的東西就是切版能力，該作品是將一個舊式網站的素材蒐集起來，改造成一個比較新型版型的網站，可以看到內容相對粗糙XD',
+    img: project_1,
+    link: 'https://adoring-newton-776ac0.netlify.app/?fbclid=IwAR39ciuRe0bRlVLSqCxtaKEoKeiRmcTbMIYr5qCbHKWcMVqCpPzdgn3_H_Y'
+  },
+  {
+    date: '2021/8 - 2021/12',
+    title: '百典創意行銷',
+    description: '在該公司主要做的是用wordpress製作、維護網站，主要是在修改CSS排版，操做介面上下架商品，對我來說，比較碰不到什麼程式面的東西，因此選擇在短時間離開。'
+  },
+  {
+    date: '2021/12 - 2022/2',
+    title: '開始學習React框架',
+    description: '因應許多工作職缺都需要框架，於是硬著頭皮開始學框架，但並沒有真正的去了解為什麼需要用框架的階段。',
+  },
+  {
+    date: '2021/1',
+    title: 'React基本作品 - todolist',
+    description: '基於一些基本的React知識，做出一個非常簡易的todolist。',
+    img: project_2,
+    link: 'https://elegant-beaver-e6642f.netlify.app/'
+  },
+  {
+    date: '2021/1',
+    title: 'React基本作品 - 串接API',
+    description: '本身常常觀看twitch這個頻台的直播，因此嘗試串閱讀twitch dev docs並串接實況主的資訊。',
+    img: project_3,
+    link: 'https://dreamy-leavitt-9706f4.netlify.app/'
+  }
+]
+
+
+
+export default changelogConfig
--- src/pages/changelog/index.tsx
+++ src/pages/changelog/index.tsx
@@ -1,35 +1,30 @@
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import Image from "next/image"
 import 'react-vertical-timeline-component/style.min.css';
-
+import changelogConfig from "./changelogConfig"
 export interface ChangelogProps {
 }
 
 export default function Changelog(props: ChangelogProps) {
+
   return (
     <div className='m-12'>
-      <VerticalTimeline layout='1-column-left'>
-        <VerticalTimelineElement date='2022/04/02'>
-          haha
-        </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          yoo
-        </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          yoo
-        </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          yoo
-        </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          yoo
-        </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          yoo
-        </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          yoo
-        </VerticalTimelineElement>
+      <VerticalTimeline >
+        <div className='flex flex-col-reverse'>
+          {changelogConfig.map((item) => {
+            const { title, date, img, description, link } = item
+            return <VerticalTimelineElement key={title} date={date} contentStyle={{ color: 'white' }}>
+              <div>
+                <h2 className='text-2xl font-bold'>{title}</h2>
+                <p className='text-black'>{description}</p>
+                <br />
+                {img && <Image src={img} alt={title} />}
+                {link && <a className='text-black underline hover:text-[#525252]' href={link} target="_blank">連結</a>}
+              </div>
+            </VerticalTimelineElement>
+          })}
+        </div>
       </VerticalTimeline>
-    </div>
+    </div >
   );
 }
